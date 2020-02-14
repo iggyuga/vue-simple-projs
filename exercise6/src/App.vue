93dfb84dd21c29262ccc18bd9f1fb27224cf901a
Ignacio Rosas <iggy.uga@gmail.com>
2020-02-14 13:18:39 -0500

    bus simple implementation

--- exercise6/src/App.vue
+++ exercise6/src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="container">
-<<<<<<< HEAD
         <app-header></app-header>
         <hr>
         <div class="row">
@@ -9,43 +8,10 @@
         </div>
         <hr>
         <app-footer></app-footer>
-=======
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
->>>>>>> d235cbbcfeff58bfb94900fbd7885bb6ea170bce
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
     import Header from './components/Shared/Header.vue';
     import Footer from './components/Shared/Footer.vue';
     import Servers from './components/Server/Servers.vue';
@@ -59,8 +25,6 @@
             'app-footer': Footer
         }
     }
-=======
->>>>>>> d235cbbcfeff58bfb94900fbd7885bb6ea170bce
 </script>
 
 <style>
