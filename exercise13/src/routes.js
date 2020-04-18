daaf0b0693c3ed1166cfe6bb40fc4f055b0b0fee
Ignacio Rosas <iggy.uga@gmail.com>
2020-04-18 09:55:23 -0400

    clean up exercise13, add exercise14

--- exercise13/src/routes.js
+++ exercise13/src/routes.js
@@ -5,22 +5,22 @@ import Header from './components/Header.vue';
 const User = resolve => {
 	require.ensure(['./components/user/User.vue'], () => {
 		resolve(require('./components/user/User.vue'));
-	});
+	}, 'user'); // 3rd param used to package modules
 };
 const UserStart = (resolve) => {
   require.ensure(["./components/user/UserStart.vue"], () => {
     resolve(require("./components/user/UserStart.vue"));
-  });
+  }, 'user');
 };
 const UserDetail = (resolve) => {
   require.ensure(["./components/user/UserDetail.vue"], () => {
     resolve(require("./components/user/UserDetail.vue"));
-  });
+  }, 'user');
 };
 const UserEdit = (resolve) => {
   require.ensure(["./components/user/UserEdit.vue"], () => {
     resolve(require("./components/user/UserEdit.vue"));
-  });
+  }, 'user');
 };
 
 export const routes = [
