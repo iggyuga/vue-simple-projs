3ad17667ad8f14b8cdbcf99fc6e78ca20eda1521
Ignacio Rosas <iggy.uga@gmail.com>
2020-04-18 13:01:02 -0400

    Vuex module, getters, mutations, actions

--- exercise14/src/App.vue
+++ exercise14/src/App.vue
@@ -3,9 +3,14 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+                <app-result></app-result>
                 <hr>
-                <app-counter @updated="counter += $event"></app-counter>
+                <app-counter></app-counter>
+                <hr>
+                <hr>
+                <app-another-result></app-another-result>
+                <hr>
+                <app-another-counter></app-another-counter>
             </div>
         </div>
     </div>
@@ -14,16 +19,16 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
+
 
     export default {
-        data() {
-            return {
-                counter: 0
-            }
-        },
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
+            appAnotherCounter: AnotherCounter,
         }
     }
 </script>
--- exercise14/store/store.js
+++ exercise14/store/store.js
@@ -3,8 +3,42 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export const store = new Vuex.store({
+export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    getters: {
+        doubleCounter: state => {
+            return state.counter * 2;
+        },
+        stringCounter: state => {
+            return state.counter + ' Clicks';
+        }
+    },
+    mutations: { //synchronously
+        increment: (state, payload) => {
+            state.counter += payload;
+        },
+        decrement: (state, payload) => {
+            state.counter -= payload;
+        }
+    },
+    actions: {
+        increment: (context, payload) => { // ({commit})
+            context.commit('increment', payload);
+        },
+        decrement: (context, payload) => { // ({commit})
+            context.commit('decrement', payload);
+        },
+        asyncIncrement: ({ commit }, payload) => {
+            setTimeout(() => {
+                commit('increment', payload.by);
+            }, payload.duration)
+        },
+        asyncDecrement: ({ commit }, payload) => {
+            setTimeout(() => {
+                commit('decrement', payload.by);
+            }, payload.duration)
+        }
     }
 })
